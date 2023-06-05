33f15e32eebb77d0db3178adf1dc3192807cb8e2
Shuyibe48 <gthrough48@gmail.com>
2023-06-05 18:03:17 +0600

    room details page done

--- src/components/Rooms/Header.jsx
+++ src/components/Rooms/Header.jsx
@@ -7,9 +7,14 @@ const Header = () => {
             <Heading
                 title='Valuvana Bali - Owl Bamboo House'
                 subtitle='Sideman, Indonesia'
-                center={true}
-
             />
+            <div className='w-full md:h-[60vh] overflow-hidden rounded-xl'>
+                <img
+                    className='object-cover w-full'
+                    src='https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg'
+                    alt='header image'
+                />
+            </div>
         </>
     );
 };
