e53fe5539037e9e568503f38004bdeb66242b9d1
Shuyibe48 <gthrough48@gmail.com>
2023-06-05 17:33:14 +0600

    category data show on category section

---
+++ src/components/Rooms/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Heading from '../Heading/Heading';
+
+const Header = () => {
+    return (
+        <>
+            <Heading
+                title='Valuvana Bali - Owl Bamboo House'
+                subtitle='Sideman, Indonesia'
+                center={true}
+
+            />
+        </>
+    );
+};
+
+export default Header;
